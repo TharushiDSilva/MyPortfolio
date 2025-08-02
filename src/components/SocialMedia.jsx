ea5addbd408f477bc4c04adb36962c89c8a888fa
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-08-02 13:48:45 +0530

    fix the issue in downloading the resume and set link to instagram

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,31 +1,3 @@
-// import { Twitter, Linkedin, Instagram, Facebook, Mail, GithubIcon, Code } from 'lucide-react';
-
-// const SocialMedia = () => {
-//   return (
-//     <div className="flex justify-center space-x-6 mt-6 z-10">
-//         <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <Linkedin size={24} />
-//       </a>
-//       <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <Twitter size={24} />
-//       </a>
-//       <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <Code size={24} />
-//       </a>
-
-//       <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <Instagram size={24} />
-//       </a>
-//       <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <GithubIcon size={24} />
-//       </a>
-//       <a href="#" className="text-white hover:text-purple-300 transition-transform transform hover:scale-110">
-//         <Mail size={24} />
-//       </a>
-//     </div>
-//   );
-// };
-
 // export default SocialMedia;
 import React from "react";
 import { Github, Linkedin, Twitter, Instagram, Code2 } from "lucide-react";
@@ -52,7 +24,7 @@ const SocialMedia = () => {
     },
     {
       icon: Instagram,
-      href: "https://instagram.com/yourusername",
+      href: "https://www.instagram.com/tharushii_d/",
       label: "Instagram",
       color: "hover:text-pink-400",
     },
