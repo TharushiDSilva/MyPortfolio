9366f00eb8836f516ad245f1869f75f872899819
TharushiDSilva <desilva.tharushiw@gmail.com>
2025-08-19 12:12:30 +0530

    Fix next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/MyPortfolio' : '',
-  assetPrefix: isProd ? '/MyPortfolio/' : '',
-  images: {
-    unoptimized: true, // needed for static export
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
